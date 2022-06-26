2c13c2d2cb336c14e40c495acabef1927987c898
Nguyen Tuong Bach <nguyentuongbach2011@gmail.com>
2022-06-26 14:43:45 +0700

    Added some class to models.py

--- models.py
+++ models.py
@@ -3,3 +3,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+class Question(models.Model):
+    pass
+class Quiz(models.Model):
+    pass
+class Answer(models.Model):
+    pass
+class Response(models.Model):
+    pass
