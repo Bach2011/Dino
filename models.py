a674c509813b2cfcb1da9db5c5759f6abb8d2e9b
Nguyen Tuong Bach <nguyentuongbach2011@gmail.com>
2022-06-27 20:01:27 +0700

    Update models.py

--- models.py
+++ models.py
@@ -8,12 +8,17 @@ class Question(models.Model):
     title = models.TextField(max_length=100)
 class Quiz(models.Model):
     questions = models.ForeignKey(Question, on_delete=CASCADE, related_name="questions")
+    owner = models.ForeignKey(User, on_delete=CASCADE)
 class Answer(models.Model):
     correct_answer = models.TextField(max_length=100)
 class Response(models.Model):
-    answer = models.TextField(max_length=100)
+    answer = models.ForeignKey(Answer, on_delete=CASCADE)
+    user = models.ForeignKey(User, on_delete=CASCADE)
 class Choices(models.Model):
     choice1 = models.TextField(max_length=100)
     choice2 = models.TextField(max_length=100)
     choice3 = models.TextField(max_length=100, blank=True, null=True)
     choice4 = models.TextField(max_length=100, blank=True, null=True)
+class RightAnswer(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=CASCADE, blank=True, null=True)
+    choice = models.ForeignKey(Choices, on_delete=CASCADE, blank=True, null=True)
