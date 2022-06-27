7d210765073d92298f13cc595ed936dcc3002d09
Nguyen Tuong Bach <nguyentuongbach2011@gmail.com>
2022-06-27 19:44:28 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,19 @@
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
 class Question(models.Model):
-    pass
+    title = models.TextField(max_length=100)
 class Quiz(models.Model):
-    pass
+    questions = models.ForeignKey(Question, on_delete=CASCADE, related_name="questions")
 class Answer(models.Model):
-    pass
+    correct_answer = models.TextField(max_length=100)
 class Response(models.Model):
-    pass
+    answer = models.TextField(max_length=100)
+class Choices(models.Model):
+    choice1 = models.TextField(max_length=100)
+    choice2 = models.TextField(max_length=100)
+    choice3 = models.TextField(max_length=100, blank=True, null=True)
+    choice4 = models.TextField(max_length=100, blank=True, null=True)
